d42825ee99853ea7bb97aef709ca59d872902bb4
jayshankar-dey <deyjayshankar@gmail.com>
2025-03-14 11:21:45 +0530

    some change

--- src/controllers/uplodeFile_our_destination.ts
+++ src/controllers/uplodeFile_our_destination.ts
@@ -1,16 +1,23 @@
-   import multer from "multer";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
     type parameter= {
        type:"destination"|"memory",
-       folder?:"uplode/" | "files/" | "images/"
        }
-  export const uplodeFile=({type='destination',folder='files/'}:parameter)=>{
+
+  const uploadDir = path.join(__dirname, "files");
+  if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+ }
+
+  export const uplodeFile=({type='destination'}:parameter)=>{
      let storage;
      
      if(type=='destination'){
        storage=multer.diskStorage({
               destination(req, file, cb) {
-                cb(null, folder);   
+                cb(null, uploadDir);   
               },
               filename(req, file, cb) {
                 cb(null, Date.now() + '-' + file.originalname);
